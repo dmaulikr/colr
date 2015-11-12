3a26d249dd46710b3fa82b1b1e43071ef84e8ded
jose31canizar <josecanizares@Isabelle.local>
2015-11-11 17:10:19 -0700

    added Block.swift

---
+++ Block.swift
@@ -0,0 +1,23 @@
+//
+//  Block.swift
+//  Rood
+//
+//  Created by Jose Canizares on 11/6/15.
+//  Copyright © 2015 rustic_republic. All rights reserved.
+//
+
+import Foundation
+
+class Block {
+    
+    var Color: Int
+    var Position: (Int, Int)
+    
+    init(Color: Int, Position: (Int, Int)) {
+        self.Color = Color
+        self.Position = Position
+    }
+    
+    //squircle centered at a,b equation: (x-a)^4 + (y-b)^4 = r^4
+    
+}
