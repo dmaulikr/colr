299b32b87da379a4deda6c218dcd16790d030077
hleise27 <Hunter@Hunters-MacBook-Pro.local>
2015-11-18 17:37:24 -0700

    Add ColorCalculator in C++ and Swift

--- ColorTheory/ColorCalculator.swift
+++ ColorTheory/ColorCalculator.swift
@@ -1,29 +1,94 @@
 //
-//  ColorCalculator.swift
-//  Rood
+//  main.swift
+//  RYBtoRGB
 //
-//  Created by Jose Canizares on 11/15/15.
-//  Copyright © 2015 rustic_republic. All rights reserved.
+//  Created by Hunter Leise on 11/11/15.
+//  Copyright © 2015 Singulariti. All rights reserved.
 //
 
 import Foundation
 
-class ColorCalculator {
-    
-    var colorArray = [Int]()
-    var Cube = [[[Int]]]()
-    
-    
-    func iterate() {
-        for (index, value) in colorArray.enumerate() {
-            print("Item \(index + 1): \(value)")
-        }
-    }
-    
-    
-    
-
+// get user input
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    return NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
 }
 
+// calculate max of 3 floating point values
+func calculateMax(r:Double, y:Double, b:Double) -> Double {
+    var max = r
+    if (y >= max) {
+        max = y
+    }
+    
+    if (b >= max) {
+        max = b
+    }
+    
+    return max
+}
 
+// calculate RGB from RYB value
+func RYBtoRGB(r:Double, y:Double, b:Double, i:Int) -> Double {
+    let white = [1.0, 1.0, 1.0]
+    let red = [1.0, 0.0, 0.0]
+    let yellow = [1.0, 1.0, 0.0]
+    let blue = [0.163, 0.373, 0.6]
+    let violet = [0.5, 0.0, 0.5]
+    let green = [0.0, 0.66, 0.2]
+    let orange = [1.0, 0.5, 0.0]
+    let black = [0.2, 0.094, 0.0]
+    
+    let rPoint: Double = 1 - r
+    let yPoint: Double = 1 - y
+    let bPoint: Double = 1 - b
+    
+    return (white[i] * rPoint * bPoint * yPoint +
+        red[i] * r * bPoint * yPoint +
+        blue[i] * rPoint * b * yPoint +
+        violet[i] * r * b * yPoint +
+        yellow[i] * rPoint * bPoint * y +
+        orange[i] * r * bPoint * y +
+        green[i] * rPoint * b * y +
+        black[i] * r * b * y) * 255
+}
 
+// Calculate RYB value from amound of each color
+func PiecesToRYB(var color:Double, max:Double) -> Double {
+    if (color == max) {
+        color = 1.0
+    } else {
+        color = (color / max)
+    }
+    return color
+}
+
+func main(){
+    var r:Double, y:Double, b:Double
+    var RGB_Red, RGB_Green, RGB_Blue: Int
+    
+    // get user input of how many pieces of each color there are
+    print("red: ")
+    r = Double(input())!
+    
+    print("yellow: ")
+    y = Double(input())!
+    
+    print("blue: ")
+    b = Double(input())!
+    
+    // calculate RYB value from pieces
+    let max = calculateMax(r, y:y, b:b)
+    r = PiecesToRYB(r, max:max)
+    y = PiecesToRYB(y, max:max)
+    b = PiecesToRYB(b, max:max)
+    
+    // calculate RGB from RYB
+    RGB_Red = Int(RYBtoRGB(r, y:y, b:b, i:0) + 0.5)
+    RGB_Green = Int(RYBtoRGB(r, y:y, b:b, i:1) + 0.5)
+    RGB_Blue = Int(RYBtoRGB(r, y:y, b:b, i:2) + 0.5)
+    
+    print("\(r) \(y) \(b)")
+    print("\(RGB_Red) \(RGB_Green) \(RGB_Blue)")
+}
